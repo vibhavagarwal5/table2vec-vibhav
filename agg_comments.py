e424f06988671072b7b0ef59c410afab56ae68fd
vibhavagarwal5 <vibhavagarwal5@gmail.com>
2019-12-05 13:47:51 +0000

    comment aggregation added

---
+++ agg_comments.py
@@ -0,0 +1,20 @@
+import pathlib
+import os
+import toml
+
+def get_comments(output_dir):
+    path = pathlib.Path(output_dir)
+    
+    comments = []
+    
+    for conf_path in path.rglob("config.toml"):
+        comment = toml.load(conf_path.resolve())["comment"]
+        comments.append((conf_path.parent.name, comment))
+    
+    
+    comments.sort(key=lambda x: list(map(int, x[0].split("_"))))
+    
+    for name, comment in comments:
+        print(f"{name}: {comment}\n")
+
+print(get_comments("./output"))
